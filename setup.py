4dbf67c7f0b27675ffa87460e889e101ecd828c5
d.anofriev <anofriev_dmitriy@mail.ru>
2023-06-11 01:42:36 +0400

    dm_api_account правки

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 REQUIRES = [
     'requests',
-    'allure-pytest'
+    'allure-pytest',
+    'pydantic',
+    'structlog'
 ]
 
 setup(
     name='dm_api_account',
     version='0.0.1',
-    packages=['dm_api_account'],
+    packages=find_packages(),
     url='https://github.com/Okkester/dm_api_account.git',
     license='MIT',
     author='d.anofriev',
     author_email='-',
     install_requires=REQUIRES,
-    description='dm_api_account with allure and logger'
+    description='Account client with allure and logger'
 )
